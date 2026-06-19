b01741fa48a638b39041b611bbd37510000f5a86
tomoss <ionut.tomoss@gmail.com>
2026-06-19 11:38:28 +0300

    Add WiFi credential storage to Storage

---
+++ iot-esp32-iaq-app/include/03_wifi/wifi_types.hpp
@@ -0,0 +1,21 @@
+#ifndef WIFI_TYPES_H
+#define WIFI_TYPES_H
+
+#include <array>
+#include <cstddef>
+
+namespace WifiTypes {
+
+constexpr size_t SSID_MAX_LEN = 33;
+constexpr size_t PASSWORD_MAX_LEN = 65;
+constexpr size_t IP_ADDR_MAX_LEN = 16;
+constexpr size_t MAC_ADDR_MAX_LEN = 18;
+
+using Ssid = std::array<char, SSID_MAX_LEN>;
+using Password = std::array<char, PASSWORD_MAX_LEN>;
+using IpAddr = std::array<char, IP_ADDR_MAX_LEN>;
+using MacAddr = std::array<char, MAC_ADDR_MAX_LEN>;
+
+} // namespace WifiTypes
+
+#endif // WIFI_TYPES_H
--- iot-esp32-iaq-app/src/02_storage/storage.cpp
+++ iot-esp32-iaq-app/src/02_storage/storage.cpp
@@ -11,3 +11,27 @@ std::optional<SensorState> Storage::loadBsecState(StorageKey p_key) {
 bool Storage::saveBsecState(StorageKey p_key, const SensorState& p_state) {
     return put(STORAGE_BSEC_NAMESPACE, p_key, p_state.data(), p_state.size());
 }
+
+std::optional<WifiTypes::Ssid> Storage::loadWifiSSID() {
+    WifiTypes::Ssid l_ssid{};
+    size_t l_len = get(STORAGE_WIFI_NAMESPACE, STORAGE_KEY_WIFI_SSID, l_ssid.data(), l_ssid.size());
+    if (l_len == 0)
+        return std::nullopt;
+    return l_ssid;
+}
+
+bool Storage::saveWifiSSID(const WifiTypes::Ssid& p_ssid) {
+    return put(STORAGE_WIFI_NAMESPACE, STORAGE_KEY_WIFI_SSID, p_ssid.data());
+}
+
+std::optional<WifiTypes::Password> Storage::loadWifiPass() {
+    WifiTypes::Password l_password{};
+    size_t l_len = get(STORAGE_WIFI_NAMESPACE, STORAGE_KEY_WIFI_PASS, l_password.data(), l_password.size());
+    if (l_len == 0)
+        return std::nullopt;
+    return l_password;
+}
+
+bool Storage::saveWifiPass(const WifiTypes::Password& p_password) {
+    return put(STORAGE_WIFI_NAMESPACE, STORAGE_KEY_WIFI_PASS, p_password.data());
+}
