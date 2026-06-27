8983c8b25f7fcb7288b180fa6e732d468bf7275b
tomoss <ionut.tomoss@gmail.com>
2026-06-27 13:34:31 +0300

    Add MqttAdapter::init() to create the underlying esp-mqtt client

--- iot-esp32-iaq-app/include/04_mqtt/mqtt_adapter.hpp
+++ iot-esp32-iaq-app/include/04_mqtt/mqtt_adapter.hpp
@@ -4,6 +4,17 @@
 #include <mqtt_client.h>
 
 class MqttAdapter {
+public:
+    MqttAdapter() = default;
+    ~MqttAdapter();
+
+    MqttAdapter(const MqttAdapter&) = delete;
+    MqttAdapter& operator=(const MqttAdapter&) = delete;
+    MqttAdapter(MqttAdapter&&) = delete;
+    MqttAdapter& operator=(MqttAdapter&&) = delete;
+
+    bool init();
+
 private:
     esp_mqtt_client_handle_t m_client = nullptr;
 };
--- iot-esp32-iaq-app/src/04_mqtt/mqtt_adapter.cpp
+++ iot-esp32-iaq-app/src/04_mqtt/mqtt_adapter.cpp
@@ -1,1 +1,25 @@
 #include "04_mqtt/mqtt_adapter.hpp"
+
+#include "00_vendor/arduino.hpp"
+
+MqttAdapter::~MqttAdapter() {
+    if (m_client) {
+        esp_mqtt_client_stop(m_client);
+        esp_mqtt_client_destroy(m_client);
+        m_client = nullptr;
+    }
+}
+
+bool MqttAdapter::init() {
+    esp_mqtt_client_config_t config{};
+    // TODO: Set up the MQTT client configuration here
+
+    if (!m_client) {
+        m_client = esp_mqtt_client_init(&config);
+        if (!m_client) {
+            Serial.println("[MQTT] esp_mqtt_client_init failed");
+            return false;
+        }
+    }
+    return true;
+}
