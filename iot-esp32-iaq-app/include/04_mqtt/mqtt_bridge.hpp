3374b0a6df02c8f98af6808f1d7184839bd78b46
tomoss <ionut.tomoss@gmail.com>
2026-07-01 14:01:36 +0300

    Subscribe to per-device command topic on MQTT connect

--- iot-esp32-iaq-app/include/04_mqtt/mqtt_bridge.hpp
+++ iot-esp32-iaq-app/include/04_mqtt/mqtt_bridge.hpp
@@ -1,6 +1,7 @@
 #ifndef MQTT_BRIDGE_HPP
 #define MQTT_BRIDGE_HPP
 
+#include <atomic>
 #include <functional>
 
 #include <mqtt_client.h>
@@ -24,29 +25,36 @@ public:
     MqttBridge& operator=(MqttBridge&&) = delete;
 
     [[nodiscard]] bool init(bool p_enableTls = false);
+
+    // Currently only called from wifi callback, so no need to be thread-safe. If called from multiple threads, make it thread-safe.
     bool connect();
     bool disconnect();
 
     void setOnConnectedCallback(OnConnectedCallback p_callback) { m_onConnectedCallback = std::move(p_callback); }
     void setOnDisconnectedCallback(OnDisconnectedCallback p_callback) { m_onDisconnectedCallback = std::move(p_callback); }
 
+    // Will be sent also if not connected, messages will be queued into the outbox and sent when connected.
     void sendSensorData(const SensorData& p_data);
+    // Will be sent only if connected, otherwise ignored. If not connected, no need to send device health data.
     void sendDeviceHealth(const DeviceHealth& p_health);
 
 private:
     void publish(const MqttTypes::Topic& p_topic, const char* p_data, int p_len);
+    void subscribe(const char* p_topic, int p_qos = 0);
 
     static void eventHandler(void* p_arg, esp_event_base_t p_base, int32_t p_eventId, void* p_eventData);
     void onEvent(esp_mqtt_event_handle_t p_event);
 
-    void handleConnected();
+    void handleConnected(bool p_sessionPresent);
     void handleDisconnected();
 
     esp_mqtt_client_handle_t m_client = nullptr;
     Storage& m_storage;
     MqttTypes::Topic m_sensorPubtopic{};
     MqttTypes::Topic m_healthPubTopic{};
+    MqttTypes::Topic m_commandSubTopic{};
     bool m_started = false;
+    std::atomic<bool> m_connected{false};
     OnConnectedCallback m_onConnectedCallback;
     OnDisconnectedCallback m_onDisconnectedCallback;
 };
