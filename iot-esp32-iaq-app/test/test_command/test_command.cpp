76b525ea020fcd8939cb6b85200f87375d45342d
Aurelian-Ionut Tomos <37960915+tomoss@users.noreply.github.com>
2026-07-02 19:36:47 +0300

    Add unit tests for command parsing (#30)

---
+++ iot-esp32-iaq-app/test/test_command/test_command.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+#include "07_utils/command.hpp"
+
+TEST(ParseCommandTest, RebootParses) {
+    EXPECT_EQ(Command::Reboot, parseCommand("reboot"));
+}
+
+TEST(ParseCommandTest, SensorLowPowerParses) {
+    EXPECT_EQ(Command::SensorLowPower, parseCommand("sensor_lp"));
+}
+
+TEST(ParseCommandTest, SensorUltraLowPowerParses) {
+    EXPECT_EQ(Command::SensorUltraLowPower, parseCommand("sensor_ulp"));
+}
+
+TEST(ParseCommandTest, UnrecognizedStringIsUnknown) {
+    EXPECT_EQ(Command::Unknown, parseCommand("not_a_command"));
+}
+
+TEST(ParseCommandTest, EmptyStringIsUnknown) {
+    EXPECT_EQ(Command::Unknown, parseCommand(""));
+}
+
+TEST(ParseCommandTest, IsCaseSensitive) {
+    EXPECT_EQ(Command::Unknown, parseCommand("Reboot"));
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
