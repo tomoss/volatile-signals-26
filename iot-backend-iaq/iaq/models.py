cf55c75bb2ade42f570d4513a015b374d37c5b08
Aurelian-Ionut Tomos <37960915+tomoss@users.noreply.github.com>
2026-07-13 14:32:15 +0300

    Add IAQ models and home view with URL routing (#34)
    * Add Device, SensorReading, and HealthReading models
    
    * Wire up iaq app URLs in project urlconf
    
    * Add IAQ home view with device list template and URL routing
    
    * Add initial migration for Device, SensorReading, and HealthReading models

--- iot-backend-iaq/iaq/models.py
+++ iot-backend-iaq/iaq/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+    mac = models.CharField(max_length=17, unique=True)
+    name = models.CharField(max_length=100, default="")
+    is_public = models.BooleanField(default=False)
+
+class SensorReading(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name="sensor_readings")
+    iaq = models.FloatField()
+    co2_equivalent = models.FloatField()
+    voc_equivalent = models.FloatField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    pressure = models.FloatField()
+    accuracy = models.PositiveBigIntegerField()
+    timestamp = models.DateTimeField()
+
+class HealthReading(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name="health_readings")
+    rssi = models.SmallIntegerField()
+    uptime = models.PositiveIntegerField()
+    heap = models.PositiveIntegerField()
+    min_heap = models.PositiveIntegerField()
+    firmware_version = models.CharField(max_length=40)
+    reset_reason = models.SmallIntegerField(default=0)
+    received_at = models.DateTimeField(auto_now_add=True)
--- iot-backend-iaq/iaq/views.py
+++ iot-backend-iaq/iaq/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from iaq.models import Device
+
+class HomeView(ListView):
+    template_name = "iaq/home.html"
+    model = Device
+
+    def get_queryset(self):
+        return Device.objects.filter(is_public=True)
+
+
