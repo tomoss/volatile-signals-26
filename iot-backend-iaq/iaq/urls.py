15475a69a4cb8a4d576e89d499a0e6cdcc7d0f06
tomoss <ionut.tomoss@gmail.com>
2026-07-16 14:28:08 +0300

    Show latest sensor reading on home page

--- iot-backend-iaq/iaq/urls.py
+++ iot-backend-iaq/iaq/urls.py
@@ -5,8 +5,10 @@ from . import views
 
 urlpatterns = [
     path("account/login/", views.IaqLoginView.as_view(), name="login"),
-    path("account/register", views.IaqRegisterView.as_view(), name="register"),
-    path("account", views.IaqAccountView.as_view(), name="account"),
-    path("logout", logout_then_login, name="logout"),
+    path("account/register/", views.IaqRegisterView.as_view(), name="register"),
+    path("account/", views.IaqAccountView.as_view(), name="account"),
+    path("device/<int:device_id>/dashboard/", views.IaqDeviceDashboardView.as_view(), name="dashboard"),
+    path("device/<int:device_id>/history/", views.IaqDeviceHistoryView.as_view(), name="history"),
+    path("logout/", logout_then_login, name="logout"),
     path("", views.IaqHomeView.as_view(), name="home"),
 ]
--- iot-backend-iaq/iaq/views.py
+++ iot-backend-iaq/iaq/views.py
@@ -3,15 +3,36 @@ from django.views.generic import CreateView, ListView, TemplateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from iaq.models import Device
+from iaq.models import Device, SensorReading
 from .forms import IaqUserCreationForm
+
+from django.db.models import OuterRef, Subquery
 
 class IaqHomeView(ListView):
     template_name = "iaq/home.html"
     model = Device
+    context_object_name = "device_list"
 
     def get_queryset(self):
-        return Device.objects.filter(is_public=True)
+        latest = SensorReading.objects.filter(device=OuterRef("pk")).order_by("-timestamp")
+
+        return Device.objects.annotate(
+            latest_iaq=Subquery(
+                latest.values("iaq")[:1]
+            ),
+            latest_timestamp=Subquery(
+                latest.values("timestamp")[:1]
+            ),
+            latest_temperature=Subquery(
+                latest.values("temperature")[:1]
+            ),
+            latest_humidity=Subquery(
+                latest.values("humidity")[:1]
+            ),
+            latest_pressure=Subquery(
+                latest.values("pressure")[:1]
+            ),
+        )
 
 class IaqLoginView(LoginView):
     template_name = "iaq/login.html"
@@ -26,4 +47,8 @@ class IaqRegisterView(CreateView):
     form_class = IaqUserCreationForm
     success_url = reverse_lazy("login")
 
+class IaqDeviceDashboardView(TemplateView):
+    template_name = "iaq/dashboard.html"
 
+class IaqDeviceHistoryView(TemplateView):
+    template_name = "iaq/history.html"
